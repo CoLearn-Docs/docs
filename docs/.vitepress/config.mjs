b908ca1b550b37ff0dd5fd5b5b84420f8c367f73
wonni <kimjaewon0901@gmail.com>
2024-07-08 13:42:54 +0900

    chore: Add favicon to site configuration

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from "vitepress";
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
+  head: [["link", { rel: "icon", href: "/favicon.ico" }]],
   outDir: "./dist",
   title: "CoLearn Docs",
   description: "CoLearn Docs",
