aefd5f990bce56050007712a1c99c69fcde77138
wonni <kimjaewon0901@gmail.com>
2024-07-12 10:11:33 +0900

    chore: Update base URL in Vitepress configuration

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from "vitepress";
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
+  base: "/",
   head: [["link", { rel: "icon", href: "/favicon.ico" }]],
   outDir: "./dist",
   title: "CoLearn Docs",
